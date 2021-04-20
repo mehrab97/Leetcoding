b8380267fa20ce6da97455b907bb3cec8a3ea20e
smaranjitghose <46641503+smaranjitghose@users.noreply.github.com>
2021-04-20 17:51:46 +0530

    Update 1295_sol5 with easier outlook

--- src/easy/1295_5.py
+++ src/easy/1295_5.py
@@ -4,8 +4,8 @@ class Solution:
         if len(nums)==0:
             return 0
         ev_d_count = 0
-        for i in nums:
-            if i != 0:
-                if math.floor(math.log10(i)) % 2 == 1:
+        for x in nums:
+            if x != 0:
+                if math.floor(math.log10(x)+1) % 2 == 0:
                     ev_d_count += 1
         return ev_d_count
